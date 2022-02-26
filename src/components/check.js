4f22825c0f914b56b62f04c9477d6f6e9b8c52cf
Pragati Gupta <73426124+pragatig746@users.noreply.github.com>
2022-02-26 23:16:00 +0530

    Delete check.js

--- src/components/check.js
+++
@@ -1,7 +0,0 @@
-const url ="https://api.spoonacular.com/recipes/findByNutrients?apiKey=a30b6a4e8070481282d392ddd8a6f220&minCarbs=10&maxCarbs=100"
-const fetch= async ()=>{
-    let data = await fetch(url)
-    let parseddata = await data.json()
-    console.log(parseddata)
-}
-
